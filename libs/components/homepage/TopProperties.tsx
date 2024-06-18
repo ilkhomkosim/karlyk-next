98b107e425fb9fc46386a020abfc6a2dfda94768
ilkhomkosim <ilkhomqosimov@gmail.com>
2024-06-18 09:52:22 +0900

    feat: develop property related like process

--- libs/components/homepage/TopProperties.tsx
+++ libs/components/homepage/TopProperties.tsx
@@ -1,3 +1,130 @@
+// import React, { useState } from 'react';
+// import { Stack, Box } from '@mui/material';
+// import useDeviceDetect from '../../hooks/useDeviceDetect';
+// import WestIcon from '@mui/icons-material/West';
+// import EastIcon from '@mui/icons-material/East';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Autoplay, Navigation, Pagination } from 'swiper';
+// import TopPropertyCard from './TopPropertyCard';
+// import { PropertiesInquiry } from '../../types/property/property.input';
+// import { Property } from '../../types/property/property';
+// import { GET_PROPERTIES } from '../../../apollo/user/query';
+// import { T } from '../../types/common';
+// import { useQuery } from '@apollo/client';
+
+// interface TopPropertiesProps {
+// 	initialInput: PropertiesInquiry;
+// }
+
+// const TopProperties = (props: TopPropertiesProps) => {
+// 	const { initialInput } = props;
+// 	const device = useDeviceDetect();
+// 	const [topProperties, setTopProperties] = useState<Property[]>([]);
+
+// 	/** APOLLO REQUESTS **/
+
+// 	const {
+// 		loading: getPropertiesLoading,
+// 		data: getPropertiesData,
+// 		error: getPropertiesError,
+// 		refetch: getPropertiesRefetch,
+// 	} = useQuery(GET_PROPERTIES, {
+// 		fetchPolicy: "cache-and-network",
+// 		variables: {input: initialInput},
+// 		notifyOnNetworkStatusChange: true,
+// 		onCompleted: (data: T) => {
+// 			setTopProperties(data?.getPropertie?.list);
+// 		}
+// 	});
+// 	/** HANDLERS **/
+
+// 	if (device === 'mobile') {
+// 		return (
+// 			<Stack className={'top-properties'}>
+// 				<Stack className={'container'}>
+// 					<Stack className={'info-box'}>
+// 						<span>Top properties</span>
+// 					</Stack>
+// 					<Stack className={'card-box'}>
+// 						<Swiper
+// 							className={'top-property-swiper'}
+// 							slidesPerView={'auto'}
+// 							centeredSlides={true}
+// 							spaceBetween={15}
+// 							modules={[Autoplay]}
+// 						>
+// 							{topProperties.map((property: Property) => {
+// 								return (
+// 									<SwiperSlide className={'top-property-slide'} key={property?._id}>
+// 										<TopPropertyCard property={property} />
+// 									</SwiperSlide>
+// 								);
+// 							})}
+// 						</Swiper>
+// 					</Stack>
+// 				</Stack>
+// 			</Stack>
+// 		);
+// 	} else {
+// 		return (
+// 			<Stack className={'top-properties'}>
+// 				<Stack className={'container'}>
+// 					<Stack className={'info-box'}>
+// 						<Box component={'div'} className={'left'}>
+// 							<span>Top properties</span>
+// 							<p>Check out our Top Properties</p>
+// 						</Box>
+// 						<Box component={'div'} className={'right'}>
+// 							<div className={'pagination-box'}>
+// 								<WestIcon className={'swiper-top-prev'} />
+// 								<div className={'swiper-top-pagination'}></div>
+// 								<EastIcon className={'swiper-top-next'} />
+// 							</div>
+// 						</Box>
+// 					</Stack>
+// 					<Stack className={'card-box'}>
+// 						<Swiper
+// 							className={'top-property-swiper'}
+// 							slidesPerView={'auto'}
+// 							spaceBetween={15}
+// 							modules={[Autoplay, Navigation, Pagination]}
+// 							navigation={{
+// 								nextEl: '.swiper-top-next',
+// 								prevEl: '.swiper-top-prev',
+// 							}}
+// 							pagination={{
+// 								el: '.swiper-top-pagination',
+// 							}}
+// 						>
+// 							{topProperties.map((property: Property) => {
+// 								return (
+// 									<SwiperSlide className={'top-property-slide'} key={property?._id}>
+// 										<TopPropertyCard property={property} />
+// 									</SwiperSlide>
+// 								);
+// 							})}
+// 						</Swiper>
+// 					</Stack>
+// 				</Stack>
+// 			</Stack>
+// 		);
+// 	}
+// };
+
+// TopProperties.defaultProps = {
+// 	initialInput: {
+// 		page: 1,
+// 		limit: 8,
+// 		sort: 'propertyRank',
+// 		direction: 'DESC',
+// 		search: {},
+// 	},
+// };
+
+// export default TopProperties;
+
+// 
+
 import React, { useState } from 'react';
 import { Stack, Box } from '@mui/material';
 import useDeviceDetect from '../../hooks/useDeviceDetect';
@@ -10,7 +137,11 @@ import { PropertiesInquiry } from '../../types/property/property.input';
 import { Property } from '../../types/property/property';
 import { GET_PROPERTIES } from '../../../apollo/user/query';
 import { T } from '../../types/common';
-import { useQuery } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
+import 'swiper/swiper-bundle.css'; // Ensure Swiper styles are imported
+import { LIKE_TARGET_PROPERTY } from '../../../apollo/user/mutation';
+import { Message } from '../../enums/common.enum';
+import { sweetMixinErrorAlert, sweetTopSmallSuccessAlert } from '../../sweetAlert';
 
 interface TopPropertiesProps {
 	initialInput: PropertiesInquiry;
@@ -22,6 +153,7 @@ const TopProperties = (props: TopPropertiesProps) => {
 	const [topProperties, setTopProperties] = useState<Property[]>([]);
 
 	/** APOLLO REQUESTS **/
+	const [likeTargetProperty] = useMutation(LIKE_TARGET_PROPERTY);
 
 	const {
 		loading: getPropertiesLoading,
@@ -30,13 +162,30 @@ const TopProperties = (props: TopPropertiesProps) => {
 		refetch: getPropertiesRefetch,
 	} = useQuery(GET_PROPERTIES, {
 		fetchPolicy: "cache-and-network",
-		variables: {input: initialInput},
+		variables: { input: initialInput },
 		notifyOnNetworkStatusChange: true,
 		onCompleted: (data: T) => {
-			setTopProperties(data?.getPropertie?.list);
+			setTopProperties(data?.getProperties?.list || []);
 		}
 	});
+
 	/** HANDLERS **/
+	const likePropertyHandler = async (user: T, id: string) => {
+		try{
+			if(!id) return;
+			if(!user._id) throw new Error(Message.NOT_AUTHENTICATED);
+
+			await likeTargetProperty({
+				variables: {input: id}
+			})
+			await getPropertiesRefetch({input: initialInput})
+
+			await sweetTopSmallSuccessAlert("success", 800)
+		} catch(err: any) {
+			console.log("ERROR, likePropertyHandler:", err.message);
+			sweetMixinErrorAlert(err.message).then();
+		}
+	}
 
 	if (device === 'mobile') {
 		return (
@@ -53,10 +202,10 @@ const TopProperties = (props: TopPropertiesProps) => {
 							spaceBetween={15}
 							modules={[Autoplay]}
 						>
-							{topProperties.map((property: Property) => {
+							{topProperties && topProperties.map((property: Property) => {
 								return (
 									<SwiperSlide className={'top-property-slide'} key={property?._id}>
-										<TopPropertyCard property={property} />
+										{/* <TopPropertyCard property={property} /> */}
 									</SwiperSlide>
 								);
 							})}
@@ -96,10 +245,10 @@ const TopProperties = (props: TopPropertiesProps) => {
 								el: '.swiper-top-pagination',
 							}}
 						>
-							{topProperties.map((property: Property) => {
+							{topProperties && topProperties.map((property: Property) => {
 								return (
 									<SwiperSlide className={'top-property-slide'} key={property?._id}>
-										<TopPropertyCard property={property} />
+										<TopPropertyCard property={property} likePropertyHandler={likePropertyHandler} />
 									</SwiperSlide>
 								);
 							})}
